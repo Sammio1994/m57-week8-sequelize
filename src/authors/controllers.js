14c1c28cf771f05a5bebd0e43b1cfd8af6c551d8
Samm Wiltshire <samm94tw@gmail.com>
2024-09-19 15:10:32 +0100

    assignment debuged

--- src/authors/controllers.js
+++ src/authors/controllers.js
@@ -3,7 +3,7 @@ const Author = require("./model");
 const getAuthorByAuthor = async (req, res) => {
     try {
         //findAll findOne
-        const author = await Author.findAll({where: {author: req.params.author}});
+        const author = await Author.findAll({where: {name: req.params.author}});
         res.status(200).json({message: "success", author: author});
     } catch (error) {
         res.status(500).json({message: error.message, error: error});
